242f850ccc14c24e64426fb1d81c7783d1a52263
Sourabh <sourabhsheokand945@gmail.com>
2024-06-03 17:31:49 +0530

    new file:   Template/admin login/index.html 	modified:   Template/login form/index.html 	modified:   Template/share page/index.html 	new file:   database/admin.json 	renamed:    login.json -> database/login.json 	modified:   logs/user_login.csv 	modified:   server.py 	new file:   static/css for sharepage/contaner.css 	new file:   static/css for sharepage/main.css

--- server.py
+++ server.py
@@ -3,15 +3,40 @@ from flask import Flask, render_template , request , redirect , url_for
 from modules.user import users_sessions
 import os
 
-with open("login.json") as file:
+################## database exposer #######################
+
+with open("database/login.json") as file:
     login_data = json.load(file)['users']
+
+with open("database/admin.json") as file:
+    admin_login = json.load(file)['admin']
+
+##########################################################
 
 app = Flask(__name__,template_folder="Template")
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-# Session(app)
 
-session = dict()
+################ web pages ################################
+
+@app.route("/")
+def home():
+    return redirect("/login")
+
+### admin login page ###
+
+@app.route("/adminlogin" , methods=["GET","POST"])
+def admin():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("pass")
+        ipaddress = request.remote_addr
+        if {username:password} in admin_login:
+            user = users_sessions.user("admin",ipaddress)
+            return redirect('.share')
+    return render_template("admin login/index.html")
+
+### member login page ###
 
 @app.route("/login",methods=["GET","POST"])
 def login():
@@ -21,21 +46,20 @@ def login():
         ipaddress = request.remote_addr
         if {username:password} in login_data:
             user = users_sessions.user(username,ipaddress)
-            return redirect(url_for(".share_content",username=user.username,session=user.session))
+            return share_content(user)
     return render_template("login form/index.html")
 
 @app.route("/share")
-def share_content():
-    try:
-        username = request.args["username"]
-        session = request.args["session"]
-    except:
-        return redirect("/login")
-    if session in users_sessions.user_id:
+def share_content(user):
+    if user.session in users_sessions.user_id:
         contents = os.listdir("./share")
-        return render_template("share page/index.html",content=contents,username=username,session=session)
+        return render_template("share page/index.html",content=contents,username=user.username,session=user.session)
     else:
         return redirect("/login")
-    
 
-app.run(debug=True) 
+
+
+
+if __name__ == "__main__":
+
+    app.run(debug=True) 
